107aec9dc4cf843bd7d9a2227e8e87829699ac3d
discphy <dinophy@nate.com>
2022-10-08 15:09:24 +0900

    feat(add): 레이싱 로직 추가

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -1,5 +1,7 @@
 package racingcar.service;
 
+import racingcar.domain.Car;
+import racingcar.domain.Racing;
 import racingcar.value.OutputMessage;
 import racingcar.domain.Cars;
 import racingcar.domain.Track;
@@ -8,15 +10,23 @@ import racingcar.dto.TrackDto;
 
 public class RacingService {
 
+    private final Carservice carservice;
     private final InputService inputService;
+    private final Racing racing;
 
     public RacingService(InputService inputService) {
+        this.carservice = new Carservice();
         this.inputService = inputService;
+        this.racing = ready();
+    }
+
+    public Racing ready() {
+        return Racing.of(initCars(), initTrack());
     }
 
     public Cars initCars() {
         try {
-            OutputService.printMessage(OutputMessage.INPUT_CAR);
+            OutputService.printInput(OutputMessage.INPUT_CAR);
             return new CarsDto(inputService.getCars()).toDomain();
         } catch (IllegalArgumentException e) {
             OutputService.inputArgumentError(e);
@@ -26,11 +36,39 @@ public class RacingService {
 
     public Track initTrack() {
         try {
-            OutputService.printMessage(OutputMessage.INPUT_TRACK);
+            OutputService.printInput(OutputMessage.INPUT_TRACK);
             return new TrackDto(inputService.getTrack()).toDomain();
         } catch (IllegalArgumentException e) {
             OutputService.inputArgumentError(e);
             return initTrack();
         }
+    }
+
+    public void start() {
+        OutputService.printExecute();
+
+        do {
+            carRacing();
+            OutputService.printLine();
+        } while (!racing.isEnd());
+
+        OutputService.printWinner(racing.getWinners());
+    }
+
+    public void carRacing() {
+        for (Car car : racing.getCars().getList()) {
+            moveCar(car);
+            winnerCar(car);
+        }
+    }
+
+    private void winnerCar(Car car) {
+        if (car.getMove() == racing.getTrack().getCount()) {
+            racing.getWinners().add(car);
+        }
+    }
+
+    private void moveCar(Car car) {
+        carservice.move(car);
     }
 }
